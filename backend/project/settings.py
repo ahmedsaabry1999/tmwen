d6621125efc5295449bc0b2d58679329d6f30874
mohamed <ahmedsaabry1999@gmail.com>
2022-03-18 17:28:39 +0200

    backend

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -130,3 +130,9 @@ STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+DEFAULT_AUTHENTICATION_CLASSES = (
+    (
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ),
+)
